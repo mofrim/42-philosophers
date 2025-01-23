7b3488f7f47427262c024481c895584eed66b1cb
mofrim <mofrim@posteo.de>
2025-01-23 20:01:54 +0100

    fix?!: nice up init_philo funcs++

--- philo/utils.c
+++ philo/utils.c
@@ -6,17 +6,11 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 09:03:17 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/01/23 14:24:01 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/01/23 15:40:58 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static int	ft_isspace(char c)
-{
-	return (c == '\f' || c == '\n' || c == '\r' || c == '\t'
-		|| c == ' ' || c == '\v');
-}
 
 static int	ft_isdigit(int c)
 {
@@ -32,7 +26,8 @@ int	ft_atoi(char *s)
 
 	num = 0;
 	sgn = 1;
-	while (ft_isspace(*s))
+	while (*s == '\f' || *s == '\n' || *s == '\r' || *s == '\t'
+		|| *s == ' ' || *s == '\v')
 		s++;
 	if (*s == '-')
 		sgn = -1;
@@ -50,4 +45,42 @@ long	gettime(void)
 
 	gettimeofday(&tv, NULL);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+int	check_invalid_params(char **av, int ac)
+{
+	int	philno;
+
+	philno = ft_atoi(av[1]);
+	if (1 > philno || philno > 1000000)
+		return (-1);
+	if (ft_atoi(av[2]) <= 0)
+		return (-1);
+	if (ft_atoi(av[3]) <= 0)
+		return (-1);
+	if (ft_atoi(av[4]) <= 0)
+		return (-1);
+	if (ac == 6)
+		if (ft_atoi(av[5]) <= 0)
+			return (-1);
+	return (0);
+}
+
+t_params	*parse_params(int ac, char **av)
+{
+	t_params	*params;
+
+	if (check_invalid_params(av, ac) == -1)
+		return (NULL);
+	params = malloc(sizeof(t_params));
+	if (params == NULL)
+		return (NULL);
+	params->philno = ft_atoi(av[1]);
+	params->ttd = ft_atoi(av[2]);
+	params->tte = ft_atoi(av[3]);
+	params->tts = ft_atoi(av[4]);
+	params->maxmeals = 0;
+	if (ac == 6)
+		params->maxmeals = ft_atoi(av[5]);
+	return (params);
 }
